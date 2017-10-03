86cf97fecb10b3ce8982437f191c8e654116f4fc
GeorgeKihara <gegohcomp@gmail.com>
2017-10-03 17:18:40 +0300

    bktlist

--- app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, BooleanField, TextAreaField, FileField
-from wtforms.validators import DataRequired, Length
-
-class UploadForm(Form):
-    image = FileField()
