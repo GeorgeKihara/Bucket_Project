f2ced3b48ba154aec67761fa23e1e29cdbadecf7
GeorgeKihara <gegohcomp@gmail.com>
2017-10-03 16:31:30 +0300

    bktlist

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 #!/usr/local/bin/python
 from app import app
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1 style="text-align:center;">Welcome to EAZYTOI</h1>
+    <p>It is currently {time}.</p>
+
+    """.format(time=the_time)
 if  __name__ == '__main__':
     app.secret_key='mysecret'
     app.run(debug=True, use_reloader=True, port=7000)
