5f987b69b200489970045c5621d0aa7b97f46540
GeorgeKihara <gegohcomp@gmail.com>
2017-10-03 17:21:28 +0300

    bktlist

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-#!/usr/local/bin/python
-from apps import app
-if  __name__ == '__main__':
-    app.secret_key='mysecret'
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1 style="text-align:center;">Welcome to EAZYTOI</h1>
+    <p>It is currently {time}.</p>
+
+    """.format(time=the_time)
+
+if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, port=8000)
-	
