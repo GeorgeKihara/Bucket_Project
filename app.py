85d43e3411d8274b502344b4e5e25dbc4e12ff6f
GeorgeKihara <gegohcomp@gmail.com>
2017-10-03 16:58:00 +0300

    bucket list web app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
-from app import app
+from apps import app
 if  __name__ == '__main__':
     app.secret_key='mysecret'
-    app.run(debug=True, port=7000)
+    app.run(debug=True, use_reloader=True, port=7000)
 	
