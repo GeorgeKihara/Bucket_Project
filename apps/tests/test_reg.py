98945df31ea179b60289edc55cbc7474a34eaf29
GeorgeKihara <gegohcomp@gmail.com>
2017-10-11 20:47:28 +0300

    json tests

--- apps/tests/test_reg.py
+++ apps/tests/test_reg.py
@@ -1,10 +1,25 @@
 import unittest
+from flask import jsonify
+from flask import Flask
+from urllib.request import urlopen
+from flask_testing import TestCase
+from flask_testing import LiveServerTestCase
 
-class BasicTests(unittest.TestCase):
+app = Flask(__name__)
+
+class MyTest(TestCase):
     
-    def test_main_page(self):
-        response = self.app.get('/', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    @app.route("/")
+    def some_json():
+        return jsonify(success=True)
+
+    def test_some_json(self):
+        response = self.client.get("/")
+        self.assertEquals(response.json, dict(success=True))
     
 if __name__ == "__main__":
     unittest.main()
