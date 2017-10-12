0b9baf0a556982b409f7ad23b3ab0c7f3d2433ca
GeorgeKihara <gegohcomp@gmail.com>
2017-10-12 10:58:16 +0300

    updated tests

--- apps/tests/test_login.py
+++ apps/tests/test_login.py
@@ -13,17 +13,11 @@ class MyTest(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
-
-    @app.route("/")
-    def some_json():
-        return jsonify(success=True)
-
-    def test_some_json(self):
-        response = self.client.get("/")
-        self.assertEquals(response.json, dict(success=True))
-
-    def test_register(self):
-        response = requests.get('http://localhost:8000/register')
     
+    def test_login(self):
+        response = requests.get('http://localhost:8000/login')
+        self.assertEqual(response.status_code, 200)
+
+
 if __name__ == "__main__":
     unittest.main()
